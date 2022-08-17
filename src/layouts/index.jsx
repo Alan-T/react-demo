19a667e00df1510ab32754112bd9286b76cf0590
zhoulf <zhouliangfei@shentie.com.cn>
2022-08-18 07:41:21 +0800

    调整部分样式

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "./navBar";
 import { Outlet, NavLink } from "react-router-dom";
+import styles from './style.module.less'
 
 const Layout = () => {
   const navColor = (isActive) => {
     return { color: isActive ? "red" : "" };
   };
   return (
-    <div className="App">
+    <div className={styles.baseContainer}>
       <NavBar />
       <h1>Welcome to React Router!</h1>
       <nav>
